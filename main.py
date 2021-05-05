0850628c3f2c0de03286f30257895cdde84514bd
Skyf0l <tom.rorato@epitech.eu>
2021-05-06 00:05:28 +0200

    Add events/projects crawler from google calendar

--- main.py
+++ main.py
@@ -20,12 +20,19 @@ CALENDAR_ID_EVENTS = config['calendarID_events']
 CALENDAR_ID_TIMELINE = config['calendarID_timeline']
 
 
-# update callendar from monday of current week
+# update callendar from monday of current week from 0 am
 if len(argv) == 2:
     current_date = datetime.strptime(argv[1], '%Y-%m-%d')
 else:
     current_date = datetime.today()
 monday_of_current_week = current_date - timedelta(days=current_date.weekday())
+monday_of_current_week = datetime(year=monday_of_current_week.year, month=monday_of_current_week.month, day=monday_of_current_week.day)
+
+google_events = get_google_events(CALENDAR_ID_EVENTS, start=monday_of_current_week)
+google_projects = get_google_projects(CALENDAR_ID_TIMELINE, start=monday_of_current_week)
+
+print(f'Already {len(google_events)} events in google calendar')
+print(f'Already {len(google_projects)} projects in google calendar')
 
 my_events = get_my_epitech_events(EPITECH_AUTOLOGIN, start=monday_of_current_week)
 
--- src/google_calendar.py
+++ src/google_calendar.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
 from src.epitech_calendar import get_epitech_event_date
+
+from datetime import datetime, timedelta
 
 # init google calendar access
 
@@ -64,3 +68,61 @@ def add_google_calendar_project(calendarID, project):
         },
     }
     service.events().insert(calendarId=calendarID, body=googleEvent).execute()
+
+
+# get events from google calendar
+
+def get_google_events(calendarID, start=None, end=None):
+    if start is None and end is None:
+        current_date = datetime.today()
+        start = current_date - timedelta(days=current_date.weekday())
+        end = start + timedelta(weeks=1)
+    elif start is None:
+        start = end - timedelta(days=31)
+    elif end is None:
+        end = start + timedelta(days=365)
+    start = start.isoformat() + 'Z'
+    end = end.isoformat() + 'Z'
+
+    events = []
+
+    page_token = None
+    while True:
+        google_events = service.events().list(calendarId=calendarID, pageToken=page_token,
+                                              timeZone='Europe/Paris', timeMin=start, timeMax=end).execute()
+        for event in google_events['items']:
+            if 'description' in event and '#event=' in event['description']:
+                events.append(event)
+        page_token = google_events.get('nextPageToken')
+        if not page_token:
+            break
+    return events
+
+
+# get projects from google calendar
+
+def get_google_projects(calendarID, start=None, end=None):
+    if start is None and end is None:
+        current_date = datetime.today()
+        start = current_date - timedelta(days=current_date.weekday())
+        end = start + timedelta(weeks=1)
+    elif start is None:
+        start = end - timedelta(days=31)
+    elif end is None:
+        end = start + timedelta(days=365)
+    start = start.isoformat() + 'Z'
+    end = end.isoformat() + 'Z'
+
+    projects = []
+
+    page_token = None
+    while True:
+        google_projects = service.events().list(calendarId=calendarID, pageToken=page_token,
+                                              timeZone='Europe/Paris', timeMin=start, timeMax=end).execute()
+        for event in google_projects['items']:
+            if 'description' in event and '#project=' in event['description']:
+                projects.append(event)
+        page_token = google_projects.get('nextPageToken')
+        if not page_token:
+            break
+    return projects
