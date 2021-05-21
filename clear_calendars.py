c3529d8ae655177a119ef720f75faf68eedc35a4
Skyf0l <tom.rorato@epitech.eu>
2021-05-21 17:23:18 +0200

    Update clear calendar script

--- clear_calendars.py
+++ clear_calendars.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
 
-import src.config as config
+import json
+import requests
 
 from src.google_calendar import *
 
-config.load_config_file('config.json')
+def get_epitech_login(epitechAutologin):
+    url = f'https://intra.epitech.eu/{epitechAutologin}/user/?format=json'
+    user_data = requests.get(url).json()
+    return user_data['login']
 
-event_deleted = clear_google_events(config.CALENDAR_ID_EVENTS)
-print(f'{event_deleted} events removed')
-event_deleted = clear_google_events(config.CALENDAR_ID_TIMELINE)
-print(f'{event_deleted} projects removed')
-event_deleted = clear_google_events(config.CALENDAR_ID_TEACHING_TEAM)
-print(f'{event_deleted} teaching events removed')
-event_deleted = clear_google_events(config.CALENDAR_ID_OTHER_CALENDARS)
-print(f'{event_deleted} events removed from other calendars')
+
+def main():
+    epitech_accounts = json.load(open('config.json'))
+    for epitech_account in epitech_accounts:
+
+        # config
+        EPITECH_AUTOLOGIN = epitech_account['epitech_autologin']
+        EPITECH_LOGIN = get_epitech_login(EPITECH_AUTOLOGIN)
+        CALENDAR_ID_EVENTS = epitech_account['calendarID_events']
+        CALENDAR_ID_TIMELINE = epitech_account['calendarID_timeline']
+        CALENDAR_ID_TEACHING_TEAM = epitech_account['calendarID_teaching_team']
+        CALENDAR_ID_OTHER_CALENDARS = epitech_account['calendarID_other_calendars']
+
+        print(f'Epitech profile: {EPITECH_LOGIN}')
+
+        if CALENDAR_ID_EVENTS is not None:
+            event_deleted = clear_google_events(CALENDAR_ID_EVENTS)
+            print(f'-> {event_deleted} events removed from calendarId_events')
+        if CALENDAR_ID_TIMELINE is not None:
+            event_deleted = clear_google_events(CALENDAR_ID_TIMELINE)
+            print(f'-> {event_deleted} events removed from calendarId_timeline')
+        if CALENDAR_ID_TEACHING_TEAM is not None:
+            event_deleted = clear_google_events(CALENDAR_ID_TEACHING_TEAM)
+            print(f'-> {event_deleted} events removed from calendarId_teaching_team')
+        if CALENDAR_ID_OTHER_CALENDARS is not None:
+            event_deleted = clear_google_events(CALENDAR_ID_OTHER_CALENDARS)
+            print(f'-> {event_deleted} events removed from calendarId_other_calendars')
+
+        print()
+
+
+if __name__ == '__main__':
+    main()
