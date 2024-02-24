b081ed47ccea90f189ff94729bc0642a46fdd82d
Alexandre Flion <huntears@kreog.com>
2024-02-24 11:27:32 +0100

    Remove useless references to autologin tokens in clear_calendar.py
    Signed-off-by: Alexandre Flion <huntears@kreog.com>

--- clear_calendars.py
+++ clear_calendars.py
@@ -16,14 +16,10 @@ def main():
     for epitech_account in epitech_accounts:
 
         # config
-        EPITECH_AUTOLOGIN = epitech_account['epitech_autologin']
-        EPITECH_LOGIN = get_epitech_login(EPITECH_AUTOLOGIN)
         CALENDAR_ID_EVENTS = epitech_account['calendarID_events']
         CALENDAR_ID_TIMELINE = epitech_account['calendarID_timeline']
         CALENDAR_ID_TEACHING_TEAM = epitech_account['calendarID_teaching_team']
         CALENDAR_ID_OTHER_CALENDARS = epitech_account['calendarID_other_calendars']
-
-        print(f'Epitech profile: {EPITECH_LOGIN}')
 
         if CALENDAR_ID_EVENTS is not None:
             event_deleted = clear_google_events(CALENDAR_ID_EVENTS, where=lambda event: event.get('description') is not None and re.search('#event=([\w-]*)!', event['description']))
